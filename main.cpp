b9e772a9156b5cd8c1a45a68dc0e19fc28e6eabe
vasya000 <vasya@gmail.com>
2024-11-30 17:32:26 +0200

    12

--- main.cpp
+++ main.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
 
+    int* arr = new int[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
 
-	return 0;
+    int sum = 0;
+    int* p = arr;
+
+    for (int i = 0; i < n; ++i) {
+        sum += *p;
+        p++;
+    }
+
+    cout << "The sum of the elements in the array is: " << sum << endl;
+
+    delete[] arr;
+
+    return 0;
 }
