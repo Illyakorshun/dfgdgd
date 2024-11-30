4907bac2ee4c46a9b44bc807b021aff7e98aa31d
vasya000 <vasya@gmail.com>
2024-11-30 17:07:10 +0200

    12

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main() {
+	int num1;
+	int num2;
+	cout << "Enter first number: ";
+	cin >> num1;
 
+	cout << "Enter second number: ";
+	cin >> num2;
+
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+
+	int ma = *ptr1 > *ptr2 ? *ptr1 : *ptr2;
+
+	cout << "The maximum number is: " << ma << endl;
 
 	return 0;
 }
