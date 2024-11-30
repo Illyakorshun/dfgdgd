6438df60775881663fad7b4a92d1573e4767c288
vasya000 <vasya@gmail.com>
2024-11-30 17:21:50 +0200

    123

--- main.cpp
+++ main.cpp
@@ -3,6 +3,37 @@
 using namespace std;
 
 int main() {
+	int num1;
+	int num2;
+	char zn;
+
+	cout << "Enter a number: ";
+	cin >> num1;
+
+	cout << "Enter a number: ";
+	cin >> num2;
+
+	cout << "Enter a simbol: ";
+	cin >> zn;
+
+
+	
+	if (zn == '+') {
+		cout << num1 + num2 << endl;
+	}
+	else if (zn == '-') {
+		cout << num1 - num2 << endl;
+	}
+	else if (zn == '*') {
+		cout << num1 * num2 << endl;
+	}
+	else if (zn == '/') {
+		cout << num1 / num2 << endl;
+	}
+	else {
+		cout << "Error" << endl;
+	}
+
 
 
 	return 0;
