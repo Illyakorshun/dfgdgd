5d338522a3ad46938583be58dcd6aadad0817f24
vasya000 <vasya@gmail.com>
2024-11-30 17:09:43 +0200

    132

--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main() {
+	int number;
+	cout << "Enter a number: ";
+	cin >> number;
 
+	int* ptr = &number;
+
+	if (*ptr > 0) {
+		cout << "The number is positive." << endl;
+	}
+	else if (*ptr < 0) {
+		cout << "The number is negative." << endl;
+	}
+	else {
+		cout << "The number is zero." << endl;
+	}
 
 	return 0;
 }
