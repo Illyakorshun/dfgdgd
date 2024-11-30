8a4a8fbcd89ca803d22fa12688abf39b12f2c571
vasya000 <vasya@gmail.com>
2024-11-30 17:12:32 +0200

    123

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
+	int a, b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
 
+	int* ptr1 = &a;
+	int* ptr2 = &b;
+
+	int rev = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = rev;
+
+	cout << "swappin: " << a << " " << b << endl;
 
 	return 0;
 }
